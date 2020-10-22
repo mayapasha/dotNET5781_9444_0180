d8bcb7769d83ef9adb94000a430c9b2cc2200556
mayapasha <mayapasha56@gmail.com>
2020-10-22 19:14:45 +0300

    Initial code+ Welcome9444

---
+++ dotNET5781_9444_0180/dotNET5781_00_9444_0180/Program9444.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET5781_00_9444_0180
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9444();
+            Welcome0180();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome0180();
+
+        private static void Welcome9444()
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
