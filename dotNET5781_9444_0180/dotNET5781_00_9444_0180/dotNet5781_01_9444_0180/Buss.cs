f61a1ab91c209b1644c270d50050aa39e40c942d
ליהי ברלב <lihibl5@gmail.com>
2020-10-31 20:14:54 +0200

    start project 01

---
+++ dotNET5781_9444_0180/dotNET5781_00_9444_0180/dotNet5781_01_9444_0180/Buss.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+	string licenseNum;
+	string startDate;
+	int mileage;
+	int fuelMileage;
+	int thisMileage;
+	bool proper()
+    {
+		if (thisMileage < 20000)
+			return true;
+		// לבדוק אם עברה שנה מאז הטיפול האחרון
+    }
+	bool fuel()// return true if the buss need fuel
+    {
+		if (fuelMileage < 1200)
+			return false;
+		else
+			return true;
+    }
+	
+}
---
+++ dotNET5781_9444_0180/dotNET5781_00_9444_0180/dotNet5781_01_9444_0180/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 
+
+namespace dotNet5781_01_9444_0180
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Buss> b;
+            char ch;
+
+            do
+            {
+                ch= (char)System.Console.Read();
+
+                switch (ch)
+                {
+                    case 'a':
+                        break;
+                    case 'b':
+                        break;
+                    case 'c':
+                        break;
+                    case 'd':
+                        break;
+                    case 'e':
+                        Console.WriteLine("bye");
+                        break;
+                    default:
+                        break;
+                }
+            } while (ch != 'e');
+           
+
+        }
+    }
+}
