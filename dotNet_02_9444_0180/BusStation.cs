131dc1a3304d33831489707972af90db4c2f4d01
ליהי ברלב <lihibl5@gmail.com>
2020-11-07 20:01:05 +0200

    i finished part 1

---
+++ dotNet_02_9444_0180/BusStation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_02_9444_0180
+{
+    class BusStation
+    {
+        public static Random r = new Random();
+        public int BusStationKey { get; set; }        
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string StationAdress { get; set; }
+        public BusStation()
+        {
+            Latitude = r.Next(31, 33);
+            Latitude += r.NextDouble();
+            Longitude = r.Next(34, 35);           
+            Longitude += r.NextDouble();
+        }
+        public override string ToString()
+        {
+            return "Bus Station Code:" + BusStationKey + " , " + Latitude + "°N " + Longitude + "°E";
+        }
+    }
+}
---
+++ dotNet_02_9444_0180/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet_02_9444_0180
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
